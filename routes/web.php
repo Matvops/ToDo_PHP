c49379ee9a8af7fa11c9054fa5335a9a258a5e32
Matheus <matheusmartinscia@gmail.com>
2025-03-26 18:36:01 -0300

    routes for update task were created

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/authLogin', [AuthController::class, 'authLogin'])->name('auth');
 
 Route::get('/home', [MainController::class, 'home'])->name('home');
+
 Route::get('/updateTask/{task_id}', [MainController::class, 'updateTask'])->name('update.task');
+Route::post('/updateTaskSubmit', [MainController::class, 'updateTaskSubmit'])->name('update.task.submit');
+
 Route::get('/createTask/{week_id}', [MainController::class, 'createTask'])->name('add.task');
 Route::post('/createTaskSubmit', [MainController::class, 'createTaskSubmit'])->name('add.task.submit');
