67016ca0c8614fcda0e8e5be60e398e805d121db
Matheus <matheusmartinscia@gmail.com>
2025-03-26 18:35:39 -0300

    update methods created on MainService

--- app/Services/MainService.php
+++ app/Services/MainService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\Task;
 use App\Models\Week;
 use Exception;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 
@@ -16,11 +17,7 @@ class MainService {
         $weeks = $this->getWeeks();
 
         if(!$tasks) {
-            return [
-                'status' => false,
-                'msg' => 'Vazio',
-                'data' => null
-            ];
+            return $this->createResponse(false, "Falha ao buscar tarefas!");
         }   
 
         $tasksAndWeeks = [
@@ -28,16 +25,11 @@ class MainService {
             'tasks' => $tasks
         ];
 
-
-        return [
-            'status' => true,
-            'msg' => 'Consulta de tasks e weeks realizadas com sucesso!',
-            'data' => $tasksAndWeeks
-        ];
-
+        return $this->createResponse(true, "Consulta de tasks e weeks realizadas com sucesso!", $tasksAndWeeks);
     }
 
-    public function createTaskSubmit(array $task){
+    public function createTaskSubmit(array $task): array
+    {
         try{
             DB::beginTransaction(); 
 
@@ -49,33 +41,70 @@ class MainService {
             $newTask->week_id = $task['week_id'];
             $newTask->user_id = session('user.user_id');
             $newTask->save();
-            DB::commit(); 
-            return [
-                'status' => true,
-                'msg' => "Tarefa criada com sucesso!",
-            ];
 
+            DB::commit(); 
+
+            return $this->createResponse(true, "Tarefa criada com sucesso!");
         } catch (Exception $e) {
+            error_log($e->getMessage());
             DB::rollBack(); 
-            return [
-                'status' => false,
-                'msg' => "Falha ao criar nova tarefa!",
-            ];
+            return $this->createResponse(false, "Falha ao criar nova tarefa!");
         }
     }
 
     public function updateTask($task_id){
+        try {
+            
+            $task = $this->getTaskByIdEncrypted($task_id);
+
+            if(!$task) {
+                return $this->createResponse(false, 'Tarefa não foi encontrada');
+            }
+
+            return $this->createResponse(true, 'Tarefa foi encontrada', $task);
+        } catch (DecryptException $e) {
+            error_log($e->getMessage());
+            return $this->createResponse(false, 'Tarefa inválida');
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            return $this->createResponse(false, 'Erro inesparado. Favor relatar ao suporte!');
+        }
+    }
+
+    public function updateTaskSubmit(array $updatedTask){
         
+        try {
+            
+            $oldTask = $this->getTaskByIdEncrypted($updatedTask['id']);
+            $newTask = $this->getOnlyTaskAttributes($updatedTask);
+
+            if(!$oldTask || !$newTask) {
+                return $this->createResponse(false, 'Tarefa não foi encontrada');
+            }
+         
+            $result = $oldTask->update($newTask);    
+            
+            if(!$result) {
+                return $this->createResponse(false, 'Não foi possível atualizar esta tarefa!');
+            }
+            return $this->createResponse(true, 'Tarefa atualizada com sucesso!');
+        } catch (DecryptException $e) {
+            error_log($e->getMessage());
+            return $this->createResponse(false, $e->getMessage());
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            return $this->createResponse(false, 'Erro inesparado. Favor relatar ao suporte!');
+        }  
     }
 
     private function getTasks(): array
     {
 
         $tasks = Task::where('user_id', session('user.user_id'))
-        ->orderByRaw('priority ASC')
-        ->with('weeks')
-        ->get()
-        ->toArray();
+            ->orderByRaw('priority ASC')
+            ->with('weeks')
+            ->get()
+            ->toArray();
 
         return $tasks;
     }
@@ -83,5 +112,32 @@ class MainService {
     private function getWeeks(): array
     {
         return Week::all()->toArray();
+    }
+
+    private function getTaskByIdEncrypted(string $id): Task
+    {
+       
+        $taskIdDecrypted = Crypt::decrypt($id);
+        $task = Task::where('id', $taskIdDecrypted)->first();
+
+        if(isset($task)) {
+            return $task;
+        } else {
+            throw new DecryptException("Erro de descriptografia");
+        }
+    }
+
+    private function getOnlyTaskAttributes(array $task): array
+    {
+        return array_intersect_key(array_diff_key($task, ['id']));
+    }
+
+    private function createResponse(bool $status, String $message, $data = null): array
+    {
+        return [
+            'status' => $status,
+            'message' => $message,
+            'data' => $data,
+        ];
     }
 }
