c9a9554d9fb729c84bffabe65dc5744153096133
Matheus <matheusmartinscia@gmail.com>
2025-03-27 20:48:14 -0300

    Logout button refactored

--- resources/views/components/logout-button.blade.php
+++ resources/views/components/logout-button.blade.php
@@ -1,6 +1,6 @@
-<div class="fixed top-5 left-5 bg-red-400 w-fit-content px-2 py-2 rounded-4xl hover:bg-red-800 transition-all duration-150">
+<div class="fixed top-5 left-5 bg-red-400 w-fit-content rounded-[50%] hover:bg-red-800 transition-all duration-200">
     <a href="{{ route('logout') }}">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-14 mx-2 my-2 hover:size-16 transition-all duration-200">
             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
         </svg>
     </a>
