92e9572c4e93350e20293d0e8371f1532b82c6b7
Matheus <matheusmartinscia@gmail.com>
2025-03-28 17:58:16 -0300

    Add middleware isAutenticated and isNotAutenticated

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,26 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
-use App\View\Components\financas;
+use App\Http\Middleware\isAutenticated;
+use App\Http\Middleware\isNotAutenticated;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/authLogin', [AuthController::class, 'authLogin'])->name('auth');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware([isAutenticated::class])->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/home', [MainController::class, 'home'])->name('home');
 
-Route::get('/home', [MainController::class, 'home'])->name('home');
+    Route::get('/updateTask/{task_id}', [MainController::class, 'updateTask'])->name('update.task');
+    Route::post('/updateTaskSubmit', [MainController::class, 'updateTaskSubmit'])->name('update.task.submit');
 
-Route::get('/updateTask/{task_id}', [MainController::class, 'updateTask'])->name('update.task');
-Route::post('/updateTaskSubmit', [MainController::class, 'updateTaskSubmit'])->name('update.task.submit');
+    Route::get('/createTask/{week_id}', [MainController::class, 'createTask'])->name('add.task');
+    Route::post('/createTaskSubmit', [MainController::class, 'createTaskSubmit'])->name('add.task.submit');
 
-Route::get('/createTask/{week_id}', [MainController::class, 'createTask'])->name('add.task');
-Route::post('/createTaskSubmit', [MainController::class, 'createTaskSubmit'])->name('add.task.submit');
+    Route::get('/delete/{task_id}', [MainController::class, 'deleteSubmit'])->name('delete.task');
 
-Route::get('/delete/{task_id}', [MainController::class, 'deleteSubmit'])->name('delete.task');
+    Route::get('/financas', [MainController::class, 'getMonthlySalary'])->name('financas');
+});
 
-Route::get('/financas', [MainController::class, 'getMonthlySalary'])->name('financas');
+Route::middleware([isNotAutenticated::class])->group(function(){
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/authLogin', [AuthController::class, 'authLogin'])->name('auth');    
+});
